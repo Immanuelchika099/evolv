8074f06029a5dfa346c19fc0e8d6bb5454165dd0
Iman <okono3672@gmail.com>
2026-09-21 20:54:41 +0100

    Clean navbar state imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import { createPortal } from 'react-dom'
 import { ArrowRight } from 'lucide-react'
 import './Navbar.css'
