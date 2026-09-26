585454af5ceb4b9726d86ea6b98c3eda64f499b6
Iman <okono3672@gmail.com>
2026-09-26 16:02:43 +0100

    Fix Your World section visibility

--- src/App.jsx
+++ src/App.jsx
@@ -641,7 +641,7 @@ function Landing({ onStart, onExplore, onArticle, onPricing, onContact }) {
           })
         })
       })
-      gsap.utils.toArray('.area').forEach((el, i) => gsap.from(el, { x: i % 2 ? 25 : -25, opacity: 0, duration: .7, ease: 'power3.out', scrollTrigger: { trigger: el, start: 'top 90%' } }))
+      gsap.utils.toArray('.area').forEach((el, i) => gsap.from(el, { x: i % 2 ? 25 : -25, duration: .7, ease: 'power3.out', scrollTrigger: { trigger: el, start: 'top 90%' } }))
       gsap.utils.toArray('.feature-card').forEach((card) => gsap.fromTo(card, { y: 90, scale: .92 }, { y: 0, scale: 1, ease: 'none', scrollTrigger: { trigger: card, start: 'top 88%', end: 'top 55%', scrub: 1.1 } }))
     }, page)
     return () => {
